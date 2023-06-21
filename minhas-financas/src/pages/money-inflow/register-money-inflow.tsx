3dbef64059b9635a35c2be4e680ab13636f6b204
Christian Oliveira <christianoliveira1908@hotmail.com>
2023-06-21 15:56:59 -0300

    Redirect to dashboard after submit the form.

--- minhas-financas/src/pages/money-inflow/register-money-inflow.tsx
+++ minhas-financas/src/pages/money-inflow/register-money-inflow.tsx
@@ -2,17 +2,21 @@ import { Container } from "react-bootstrap"
 import FormMoneyInflow from "../../components/forms/form-money-inflow"
 import MoneyInflowDto from "../../dto/MoneyInflowDto"
 import MoneyInflowClient from "../../client/MoneyInflowClient";
+import { useNavigate } from "react-router-dom";
 
 interface IProps {
     client: MoneyInflowClient;
 }
 
 const RegisterMoneyInflow = ({ client }: IProps) => {
+    const navigate = useNavigate();
 
     const onSubmit = async (data: MoneyInflowDto) => {
         await client.register(data);
 
         alert("Entrada de dinheiro registrada com sucesso!");
+
+        navigate("/Dashboard");
     }
 
     return (
--- minhas-financas/src/pages/money-outflow/register-money-outflow.tsx
+++ minhas-financas/src/pages/money-outflow/register-money-outflow.tsx
@@ -1,19 +1,24 @@
 import { Container } from "react-bootstrap";
 import FormMoneyOutflow from "../../components/forms/form-money-outflow";
 import MoneyOutflowClient from "../../client/MoneyOutflowClient";
+import { useNavigate } from "react-router-dom";
 
 interface IProps {
     client: MoneyOutflowClient
 }
 
 const RegisterMoneyOutflow = ({ client }: IProps) => {
+    const navigate = useNavigate();
 
     const onSubmit = async (moneyOutflow: MoneyOutflowDto) => {
         try{            
             await client.register(
                 moneyOutflow
             );
+            
             alert("Saída de dinheiro registrado com sucesso!");
+
+            navigate("/Dashboard");
         }
         catch(err){
             alert("Não possível realizar o registro da saída");
